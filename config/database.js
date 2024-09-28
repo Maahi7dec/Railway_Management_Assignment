446a47a3f3f13cfac74978c519ebdf7606a045e5
Maahi7dec <166039869+Maahi7dec@users.noreply.github.com>
2024-09-28 11:06:27 +0530

    database.js

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,     // Database name
-  process.env.DB_USER,     // Username
-  process.env.DB_PASSWORD, // Password
+  process.env.DB_NAME,     
+  process.env.DB_USER,     
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
