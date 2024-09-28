d02ecb43a91616bb371ad48aabed8da9a61d9641
Maahi7dec <166039869+Maahi7dec@users.noreply.github.com>
2024-09-28 11:09:27 +0530

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sequelize = require('./config/database');
 require('dotenv').config();
 
-// Import Routes
+
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
 const bookingRoutes = require('./routes/Booking');
@@ -10,21 +10,21 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
-// Middleware to parse JSON
+
 app.use(express.json());
 
-// Routes
+
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/Bookings', bookingRoutes);
 
-// Test Route
+
 app.get('/', (req, res) => {
   res.send('Railway Management System API');
 });
 
-// Sync Database and Start Server
+
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync({ alter: true }) // Use { force: true } for development to reset DB
